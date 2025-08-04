8512509f5ac607b43018a01828798eec342e546a
AlexandraZambrano <alezambrano03@gmail.com>
2025-08-04 16:19:02 +0200

    feat: Added Editor state manage and initial toolbar functionality #2

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "./editor";
+import Toolbar from "./toolbar";
 
 interface DocuementIdPageProps {
     params:Promise<{documentId: string}>;
@@ -9,6 +10,7 @@ const DocumentIdPage = async({ params }: DocuementIdPageProps) => {
     const documentId = awaitedParams.documentId;
     return ( 
         <div className="min-h-screen bg-[#FAFBFD]">
+        <Toolbar />
         <Editor />
         </div>
      );
