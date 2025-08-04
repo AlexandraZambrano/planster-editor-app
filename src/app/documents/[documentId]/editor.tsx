c64a0da4d773f0ef692e976d0bf3aa9637e258f6
AlexandraZambrano <alezambrano03@gmail.com>
2025-08-04 10:36:52 +0200

    feature: Added tiptap editor dependency

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+
+export const Editor = () => {
+
+    const editor = useEditor({
+    extensions: [StarterKit],
+    content: '<p>Hello World! 🌎️</p>',
+    immediatelyRender: false,
+  })
+
+    return ( 
+        <div>
+            <EditorContent editor={editor} />
+        </div>
+     );
+}
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import { Editor } from "./editor";
+
 interface DocuementIdPageProps {
     params:Promise<{documentId: string}>;
 };
@@ -6,7 +8,9 @@ const DocumentIdPage = async({ params }: DocuementIdPageProps) => {
     const awaitedParams = await params;
     const documentId = awaitedParams.documentId;
     return ( 
-        <div>Document ID: {documentId}</div>
+        <div>Document ID: {documentId}
+        <Editor />
+        </div>
      );
 }
  
